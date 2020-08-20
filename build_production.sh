1b27634b499469f6ac4df02bffb73109c865c6ef
Rainer Gevers <rainergeve@gmail.com>
2020-08-20 20:18:15 +0200

    updated production build

--- build_production.sh
+++ build_production.sh
@@ -1,3 +1,5 @@
-export version=$(cat VERSION | grep "^VERSION=" | cut -d'=' -f 2 | awk '{print $1}' | tr -d '\040\011\012\015' | tr -d \r)
-docker build -t rainerza/tasker:latest -t rainerza/tasker:0.0.5 .
-#docker push rainerza/tasker:latest rainerza/tasker:"$(version | tr -d "\r")" .
+export version=$(cat VERSION | awk '{print $1}' | tr -d '\040\011\012\015' | tr -d \r\n)
+docker build -t rainerza/tasker:latest -t rainerza/tasker:$version .
+
+docker push rainerza/tasker:latest
+docker push rainerza/tasker:$version
