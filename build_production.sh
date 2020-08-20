cd0fa556be04b8bce82ed9975b16f82e062b772a
Rainer Gevers <rainergeve@gmail.com>
2020-08-20 20:01:24 +0200

    idk, vscode wanted this

--- build_production.sh
+++ build_production.sh
@@ -1,3 +1,3 @@
 export version=$(cat VERSION | grep "^VERSION=" | cut -d'=' -f 2 | awk '{print $1}' | tr -d '\040\011\012\015' | tr -d \r)
-docker build -t rainerza/tasker:latest -t rainerza/tasker:0.0.2 .
+docker build -t rainerza/tasker:latest -t rainerza/tasker:0.0.5 .
 #docker push rainerza/tasker:latest rainerza/tasker:"$(version | tr -d "\r")" .
